2a7561afa40d0ae87d2155bc9e0fe27fe925d9dd
vadim <asikpro13@mail.ru>
2020-12-05 20:38:55 +0500

    all

--- DataBase/workFromDB.py
+++ DataBase/workFromDB.py
@@ -2,7 +2,7 @@ import sqlite3  # Работа с бд
 #  Импорт всех нужных библиотек, стилей
 
 
-def db(zapr, *args):  # Функция для работы с базой данных
+def db(zapr, *args):  # Функция для работы с базой данных.
     connect = sqlite3.connect('../DataBase/auth.db')  # Создаем соединение
     cun = connect.cursor()  # Создаем курсор
     result = cun.execute(zapr, *args).fetchall()  # ->
